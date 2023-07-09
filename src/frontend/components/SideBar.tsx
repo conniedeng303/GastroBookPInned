fe110e3930cf86b88863a737a2a2d47ac9875a4f
Ronald Ng <ronaldn1936@gmail.com>
2023-07-09 01:39:53 -0400

    side bar fix

--- src/frontend/components/SideBar.tsx
+++ src/frontend/components/SideBar.tsx
@@ -4,7 +4,7 @@ import SearchBar from "./SearchBar";
 function SideBar() {
   return (
     <>
-      <div className="vh-100 w-25 p-3 mb-2 bg-info text-dark rounded-4">
+      <div className="vh-100 w-25 p-3 mb-2 bg-info text-dark">
         <h1 className="text-center">GastroBook</h1>
         <div className="mt-5">
           <SearchBar />
