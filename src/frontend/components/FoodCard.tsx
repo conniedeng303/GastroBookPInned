ee5c52a8c36d32376cdecdd153a2a08b1d8aa18a
Connie <conniedeng303@gmail.com>
2023-07-11 22:06:04 -0400

    styled the foodcard a bit

--- src/frontend/components/FoodCard.tsx
+++ src/frontend/components/FoodCard.tsx
@@ -1,4 +1,5 @@
-// import TimeLogo from "../assets/time_card.svg"
+import "../FoodCard.css";
+
 interface Recipe {
   recipe: string;
   image?: string;
@@ -11,17 +12,17 @@ function FoodCard({ recipe }: Recipe) {
   // ]
 
   return (
-    <div className="card m-2" style={{ width: "97%", height: "300px"}}>
+    <div className="card d-flex justify-content-center m-2" style={{ width: "97%", height: "700px"}}>
       <div className="card_body">
-        {/* <img
-          className="card-img-top"
+        <img
+          className="img, img-top"
           src="https://www.howtogame.net/wp-content/uploads/2023/05/how-tall-is-link-totk-1024x576.jpg"
           alt=""
-        /> */}
+        />
         <h2 className="card__description"></h2>
-        <p className="card__description">description of card {recipe}</p>
+        <p className="card__description text-center">description of card {recipe}</p>
       </div>
-      <button className="card__bttn">View Recipe {recipe}</button>
+      <button className="card__bttn btn-block">View Recipe {recipe}</button>
     </div>
   );
 }
